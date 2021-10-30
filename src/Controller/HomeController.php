cc5dc8c761d31922e152bf3f6a191d79e7ab4a71
glerique <gael.lerique@gmail.com>
2021-10-30 10:13:36 +0200

    Update HomeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,19 +2,21 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ChaletRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function index(): Response
+    public function index(ChaletRepository $chaletRepository): Response
     {
+        $chalets = $chaletRepository->findBy([], [], 3);
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'chalets' => $chalets
         ]);
     }
 }
